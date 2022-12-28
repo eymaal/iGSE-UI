2b2acf7b858b2f130d9cbfff8e8a900e005e52b9
Amal Jos <ajc99@student.le.ac.uk>
2022-12-28 19:12:23 +0000

    temp

--- src/pages/Login-Signup/Registration.svelte
+++ src/pages/Login-Signup/Registration.svelte
@@ -44,6 +44,7 @@
       <h1 class="text-5xl font-bold">Get Started!</h1>
       <p class="py-6">Enter your details and become a part of the Great Shangri-La Energy Company!</p>
     </div>
+
     <form id="registration" class="contents" on:submit|preventDefault={register}>
       <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <div class="card-body">
@@ -52,7 +53,7 @@
             <label for="email" class="label">
               <span class="label-text">Email</span>
             </label>
-            <input required type="email" placeholder="Enter your email address" class="input input-bordered required:border-red-500" name="email" bind:value={customer_id}/>
+            <input required type="email" placeholder="Enter your email address" class="peer input input-bordered" name="email" bind:value={customer_id}/>
           </div>
 
           <div class="form-control">
@@ -66,14 +67,14 @@
               <label for="address" class="label">
                 <span class="label-text">Address</span>
               </label>
-              <input type="text" placeholder="Enter your address" class="input input-bordered" name="address" bind:value={address}/>
+              <input required type="text" placeholder="Enter your address" class="input input-bordered" name="address" bind:value={address}/>
           </div>
 
           <div class="form-control">
             <label for="property_type" class="label">
               <span class="label-text">Property Type</span>
             </label>
-            <select class="select select-bordered w-full max-w-xs font-normal" bind:value={property_type}>
+            <select required class="select select-bordered w-full max-w-xs font-normal" bind:value={property_type}>
               <option disabled selected>Select your property type</option>
               {#each property_types as type}
                 <option value="{type}">{type}</option>
@@ -85,14 +86,14 @@
             <label for="bedrooms" class="label">
               <span class="label-text">Number of Bedrooms</span>
             </label>
-            <input type="number" placeholder="Enter number of bedrooms" class="input input-bordered" name="bedrooms" min=0 bind:value={bedroom_num}/>
+            <input required type="number" placeholder="Enter number of bedrooms" class="input input-bordered" name="bedrooms" min=0 bind:value={bedroom_num}/>
           </div>
 
           <div class="form-control">
             <label for="evoucher" class="label">
               <span class="label-text">Energy Voucher code</span>
             </label>
-            <input type="text" placeholder="Enter 8 digit code" class="input input-bordered" name="evoucher" bind:value={EVC_code}/>
+            <input required type="text" placeholder="Enter 8 digit code" class="input input-bordered" name="evoucher" bind:value={EVC_code}/>
           </div>
       
       
