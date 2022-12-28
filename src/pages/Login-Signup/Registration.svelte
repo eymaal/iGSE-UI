4e028d69cdf9cf27a9fa87ac6a20b0bc126f8fb4
Amal Jos <ajc99@student.le.ac.uk>
2022-12-28 01:41:40 +0000

    Registration - Happy Path

--- src/pages/Login-Signup/Registration.svelte
+++ src/pages/Login-Signup/Registration.svelte
@@ -1,7 +1,6 @@
 <script>
-  const registerData = {
-    // 'name':"",
-  };
+  let result = null;
+  let customer_id, password_hash, property_type, address, bedroom_num, EVC_code, balance=200.0, type="user";
   let property_types = [
     'detached',
     'terrace',
@@ -12,9 +11,26 @@
     'mansion',
   ]
 
-  function register(){
-    if(registerData)
-    alert(JSON.stringify(registerData));
+  async function register(){
+    const res = await fetch('http://localhost:8080/iGSE/register',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        customer_id,
+        password_hash,
+        address,
+        property_type,
+        bedroom_num,
+        balance,
+        type
+      })
+    })
+    
+    const json = await res.json();
+    result = JSON.stringify(json);
+    alert(result);
   }
 
   function onUpdate(event){
@@ -36,35 +52,28 @@
             <label for="email" class="label">
               <span class="label-text">Email</span>
             </label>
-            <input required type="email" placeholder="Enter your email address" class="input input-bordered required:border-red-500" name="email" bind:value={registerData.email}/>
+            <input required type="email" placeholder="Enter your email address" class="input input-bordered required:border-red-500" name="email" bind:value={customer_id}/>
           </div>
 
           <div class="form-control">
             <label for="password" class="label">
               <span class="label-text">Password</span>
             </label>
-            <input required type="password" placeholder="Enter your password" class="input input-bordered" name="password" bind:value={registerData.password}/>
-          </div>
-          
-          <div class="form-control">
-              <label for="name" class="label">
-                <span class="label-text">Name</span>
-              </label>
-              <input type="text" placeholder="Enter your name" class="input input-bordered" name="name" bind:value={registerData.name}/>
+            <input required type="password" placeholder="Enter your password" class="input input-bordered" name="password" bind:value={password_hash}/>
           </div>
       
           <div class="form-control">
               <label for="address" class="label">
                 <span class="label-text">Address</span>
               </label>
-              <input type="text" placeholder="Enter your address" class="input input-bordered" name="address" bind:value={registerData.address}/>
+              <input type="text" placeholder="Enter your address" class="input input-bordered" name="address" bind:value={address}/>
           </div>
 
           <div class="form-control">
             <label for="property_type" class="label">
               <span class="label-text">Property Type</span>
             </label>
-            <select class="select select-bordered w-full max-w-xs font-normal" bind:value={registerData.propertyType}>
+            <select class="select select-bordered w-full max-w-xs font-normal" bind:value={property_type}>
               <option disabled selected>Select your property type</option>
               {#each property_types as type}
                 <option value="{type}">{type}</option>
@@ -76,14 +85,14 @@
             <label for="bedrooms" class="label">
               <span class="label-text">Number of Bedrooms</span>
             </label>
-            <input type="number" placeholder="Enter number of bedrooms" class="input input-bordered" name="bedrooms" min=0 bind:value={registerData.numberOfBedrooms}/>
+            <input type="number" placeholder="Enter number of bedrooms" class="input input-bordered" name="bedrooms" min=0 bind:value={bedroom_num}/>
           </div>
 
           <div class="form-control">
             <label for="evoucher" class="label">
               <span class="label-text">Energy Voucher code</span>
             </label>
-            <input type="text" placeholder="Enter 8 digit code" class="input input-bordered" name="evoucher" bind:value={registerData.evc}/>
+            <input type="text" placeholder="Enter 8 digit code" class="input input-bordered" name="evoucher" bind:value={EVC_code}/>
           </div>
       
       
