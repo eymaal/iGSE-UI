544a9e013f247f4c366684de7a4c9cf1710f7a70
Amal Jos <ajc99@student.le.ac.uk>
2023-01-16 09:01:58 +0000

    graph for admin 3 in-progress

--- src/pages/Dashboard/util/AdminGraph.svelte
+++ src/pages/Dashboard/util/AdminGraph.svelte
@@ -22,9 +22,64 @@
             console.log(data);
         })
     }
+      
 
-    
-    
+
+    import FusionCharts from 'fusioncharts';
+    import Charts from 'fusioncharts/fusioncharts.charts';
+    import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+    import SvelteFC, { fcRoot } from 'svelte-fusioncharts';
+
+    // Always set FusionCharts as the first parameter
+    fcRoot(FusionCharts, Charts, FusionTheme);
+
+    const dataSource = {
+    "chart": {
+        "caption": "Average Gas and Electricity Consumption per day",
+        "yaxisname": "Consumption in kWh",
+        "subcaption": "(in kWh)",
+        "flatscrollbars": "0",
+        "scrollheight": "12",
+        "numvisibleplot": "8",
+        "plottooltext": "<b>$dataValue</b> $seriesName consumption by $label",
+        "theme": "fusion"
+    },
+    "categories": [
+        {
+        "category": [
+            {
+            "label": "1994"
+            },
+            {
+            "label": "1995"
+            }            
+        ]
+        }
+    ],
+    "dataset": [
+        {
+        "seriesname": "Diptera",
+        "data": [
+            {
+            "value": "3622"
+            },
+            {
+            "value": "2612"
+            }            
+        ]
+        }
+    ]
+    };
+
+    const chartConfigs = {
+    type: 'scrollstackedcolumn2d',
+    width: 600,
+    height: 400,
+    dataFormat: 'json',
+    dataSource
+    };
 </script>
 
-<div id="chart"></div>
+<div id="container">
+    <SvelteFC {...chartConfigs} />
+  </div>
